45f3a3d7824458bcd85c41f652427af9a4796828
André Freitas <p.andrefreitas@gmail.com>
2024-02-01 12:05:56 +0000

    using a base container

--- main.py
+++ main.py
@@ -5,22 +5,31 @@ from dependency_injector import containers, providers
 from dependency_injector.wiring import Provide, inject
 
 
-async def init_foo():
-    raise Exception("Some error")
-    print("Foo: init")
-    yield {"bar": "foo"}
-    print("Foo: shutting down")
+async def raise_exception():
     await asyncio.sleep(1)
+    raise Exception("Some error")
 
 
-class Container(containers.DeclarativeContainer):
-    foo = providers.Resource(init_foo)
+async def init_foo(value: str):
+    await raise_exception()
+    yield {"bar": value}
+
+
+class BaseContainer(containers.DeclarativeContainer):
+    foo = providers.Resource(init_foo, "some-value")
+
+
+class WorkerContainer(containers.DeclarativeContainer):
+    base = providers.Container(BaseContainer)
 
 
 @inject
-async def main(foo=Provide[Container.foo], container=Provide[Container]):
+async def main(
+    container=Provide[WorkerContainer], foo=Provide[WorkerContainer.base.foo]
+):
     await container.init_resources()
-    print(f"foo {foo}")
+
+    print("foo", foo)
 
     for _ in range(5):
         print("working and sleeping...")
@@ -30,6 +39,6 @@ async def main(foo=Provide[Container.foo], container=Provide[Container]):
 
 
 if __name__ == "__main__":
-    container = Container()
-    container.wire(modules=[__name__])
+    worker_container = WorkerContainer()
+    worker_container.wire(modules=[__name__])
     asyncio.run(main())
